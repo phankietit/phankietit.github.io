b5ca0c5c245769ee8a5d93d39db466cb6ee8a4cc
Phan Kiet <phankiet.it@gmail.com>
2023-10-02 18:41:15 +0700

    update home

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
  *
  * ------------------------------------------------------------------- */
 
-(function(html) {
+(function (html)
+{
 
     "use strict";
 
@@ -11,74 +12,79 @@
 
 
 
-   /* Animations
-    * -------------------------------------------------- */
-    const tl = anime.timeline( {
+    /* Animations
+     * -------------------------------------------------- */
+    const tl = anime.timeline({
         easing: 'easeInOutCubic',
         duration: 800,
         autoplay: false
     })
-    .add({
-        targets: '#loader',
-        opacity: 0,
-        duration: 1000,
-        begin: function(anim) {
-            window.scrollTo(0, 0);
-        }
-    })
-    .add({
-        targets: '#preloader',
-        opacity: 0,
-        complete: function(anim) {
-            document.querySelector("#preloader").style.visibility = "hidden";
-            document.querySelector("#preloader").style.display = "none";
-        }
-    })
-    .add({
-        targets: '.s-header',
-        translateY: [-100, 0],
-        opacity: [0, 1]
-    }, '-=200')
-    .add({
-        targets: [ '.s-intro .text-pretitle', '.s-intro .text-huge-title'],
-        translateX: [100, 0],
-        opacity: [0, 1],
-        delay: anime.stagger(400)
-    })
-    .add({
-        targets: '.circles span',
-        keyframes: [
-            {opacity: [0, .3]},
-            {opacity: [.3, .1], delay: anime.stagger(100, {direction: 'reverse'})}
-        ],
-        delay: anime.stagger(100, {direction: 'reverse'})
-    })
-    .add({
-        targets: '.intro-social li',
-        translateX: [-50, 0],
-        opacity: [0, 1],
-        delay: anime.stagger(100, {direction: 'reverse'})
-    })
-    .add({
-        targets: '.intro-scrolldown',
-        translateY: [100, 0],
-        opacity: [0, 1]
-    }, '-=800');
+        .add({
+            targets: '#loader',
+            opacity: 0,
+            duration: 1000,
+            begin: function (anim)
+            {
+                window.scrollTo(0, 0);
+            }
+        })
+        .add({
+            targets: '#preloader',
+            opacity: 0,
+            complete: function (anim)
+            {
+                document.querySelector("#preloader").style.visibility = "hidden";
+                document.querySelector("#preloader").style.display = "none";
+            }
+        })
+        .add({
+            targets: '.s-header',
+            translateY: [-100, 0],
+            opacity: [0, 1]
+        }, '-=200')
+        .add({
+            targets: ['.s-intro .text-pretitle', '.s-intro .text-huge-title'],
+            translateX: [100, 0],
+            opacity: [0, 1],
+            delay: anime.stagger(400)
+        })
+        .add({
+            targets: '.circles span',
+            keyframes: [
+                { opacity: [0, .3] },
+                { opacity: [.3, .1], delay: anime.stagger(100, { direction: 'reverse' }) }
+            ],
+            delay: anime.stagger(100, { direction: 'reverse' })
+        })
+        .add({
+            targets: '.intro-social li',
+            translateX: [-50, 0],
+            opacity: [0, 1],
+            delay: anime.stagger(100, { direction: 'reverse' })
+        })
+        .add({
+            targets: '.intro-scrolldown',
+            translateY: [100, 0],
+            opacity: [0, 1]
+        }, '-=800');
 
 
 
-   /* Preloader
-    * -------------------------------------------------- */
-    const ssPreloader = function() {
+    /* Preloader
+     * -------------------------------------------------- */
+    const ssPreloader = function ()
+    {
 
         const preloader = document.querySelector('#preloader');
         if (!preloader) return;
-        
-        window.addEventListener('load', function() {
+
+        window.addEventListener('load', function ()
+        {
             document.querySelector('html').classList.remove('ss-preload');
             document.querySelector('html').classList.add('ss-loaded');
 
-            document.querySelectorAll('.ss-animated').forEach(function(item){
+            document.querySelectorAll('.ss-animated').forEach(function (item)
+            {
                 item.classList.remove('ss-animated');
             });
 
@@ -93,9 +99,10 @@
     }; // end ssPreloader
 
 
-   /* Mobile Menu
-    * ---------------------------------------------------- */ 
-    const ssMobileMenu = function() {
+    /* Mobile Menu
+     * ---------------------------------------------------- */
+    const ssMobileMenu = function ()
+    {
 
         const toggleButton = document.querySelector('.mobile-menu-toggle');
         const mainNavWrap = document.querySelector('.main-nav-wrap');
@@ -103,27 +110,33 @@
 
         if (!(toggleButton && mainNavWrap)) return;
 
-        toggleButton.addEventListener('click', function(event) {
+        toggleButton.addEventListener('click', function (event)
+        {
             event.preventDefault();
             toggleButton.classList.toggle('is-clicked');
             siteBody.classList.toggle('menu-is-open');
         });
 
-        mainNavWrap.querySelectorAll('.main-nav a').forEach(function(link) {
-            link.addEventListener("click", function(event) {
+        mainNavWrap.querySelectorAll('.main-nav a').forEach(function (link)
+        {
+            link.addEventListener("click", function (event)
+            {
 
                 // at 800px and below
-                if (window.matchMedia('(max-width: 800px)').matches) {
+                if (window.matchMedia('(max-width: 800px)').matches)
+                {
                     toggleButton.classList.toggle('is-clicked');
                     siteBody.classList.toggle('menu-is-open');
                 }
             });
         });
 
-        window.addEventListener('resize', function() {
+        window.addEventListener('resize', function ()
+        {
 
             // above 800px
-            if (window.matchMedia('(min-width: 801px)').matches) {
+            if (window.matchMedia('(min-width: 801px)').matches)
+            {
                 if (siteBody.classList.contains('menu-is-open')) siteBody.classList.remove('menu-is-open');
                 if (toggleButton.classList.contains("is-clicked")) toggleButton.classList.remove("is-clicked");
             }
@@ -132,36 +145,41 @@
     }; // end ssMobileMenu
 
 
-   /* Highlight active menu link on pagescroll
-    * ------------------------------------------------------ */
-    const ssScrollSpy = function() {
+    /* Highlight active menu link on pagescroll
+     * ------------------------------------------------------ */
+    const ssScrollSpy = function ()
+    {
 
         const sections = document.querySelectorAll(".target-section");
 
         // Add an event listener listening for scroll
         window.addEventListener("scroll", navHighlight);
 
-        function navHighlight() {
-        
+        function navHighlight()
+        {
+
             // Get current scroll position
             let scrollY = window.pageYOffset;
-        
+
             // Loop through sections to get height(including padding and border), 
             // top and ID values for each
-            sections.forEach(function(current) {
+            sections.forEach(function (current)
+            {
                 const sectionHeight = current.offsetHeight;
                 const sectionTop = current.offsetTop - 50;
                 const sectionId = current.getAttribute("id");
-            
-               /* If our current scroll position enters the space where current section 
-                * on screen is, add .current class to parent element(li) of the thecorresponding 
-                * navigation link, else remove it. To know which link is active, we use 
-                * sectionId variable we are getting while looping through sections as 
-                * an selector
-                */
-                if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+
+                /* If our current scroll position enters the space where current section 
+                 * on screen is, add .current class to parent element(li) of the thecorresponding 
+                 * navigation link, else remove it. To know which link is active, we use 
+                 * sectionId variable we are getting while looping through sections as 
+                 * an selector
+                 */
+                if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+                {
                     document.querySelector(".main-nav a[href*=" + sectionId + "]").parentNode.classList.add("current");
-                } else {
+                } else
+                {
                     document.querySelector(".main-nav a[href*=" + sectionId + "]").parentNode.classList.remove("current");
                 }
             });
@@ -170,19 +188,22 @@
     }; // end ssScrollSpy
 
 
-   /* Animate elements if in viewport
-    * ------------------------------------------------------ */
-    const ssViewAnimate = function() {
+    /* Animate elements if in viewport
+     * ------------------------------------------------------ */
+    const ssViewAnimate = function ()
+    {
 
         const blocks = document.querySelectorAll("[data-animate-block]");
 
         window.addEventListener("scroll", viewportAnimation);
 
-        function viewportAnimation() {
+        function viewportAnimation()
+        {
 
             let scrollY = window.pageYOffset;
 
-            blocks.forEach(function(current) {
+            blocks.forEach(function (current)
+            {
 
                 const viewportHeight = window.innerHeight;
                 const triggerTop = (current.offsetTop + (viewportHeight * .2)) - viewportHeight;
@@ -191,15 +212,17 @@
                 const inView = scrollY > triggerTop && scrollY <= blockSpace;
                 const isAnimated = current.classList.contains("ss-animated");
 
-                if (inView && (!isAnimated)) {
+                if (inView && (!isAnimated))
+                {
                     anime({
                         targets: current.querySelectorAll("[data-animate-el]"),
                         opacity: [0, 1],
                         translateY: [100, 0],
-                        delay: anime.stagger(400, {start: 200}),
+                        delay: anime.stagger(400, { start: 200 }),
                         duration: 800,
                         easing: 'easeInOutCubic',
-                        begin: function(anim) {
+                        begin: function (anim)
+                        {
                             current.classList.add("ss-animated");
                         }
                     });
@@ -210,9 +233,10 @@
     }; // end ssViewAnimate
 
 
-   /* Swiper
-    * ------------------------------------------------------ */ 
-    const ssSwiper = function() {
+    /* Swiper
+     * ------------------------------------------------------ */
+    const ssSwiper = function ()
+    {
 
         const mySwiper = new Swiper('.swiper-container', {
 
@@ -238,28 +262,33 @@
                     spaceBetween: 80
                 }
             }
-         });
+        });
 
     }; // end ssSwiper
 
 
-   /* Lightbox
-    * ------------------------------------------------------ */
-    const ssLightbox = function() {
+    /* Lightbox
+     * ------------------------------------------------------ */
+    const ssLightbox = function ()
+    {
 
         const folioLinks = document.querySelectorAll('.folio-list__item-link');
         const modals = [];
 
-        folioLinks.forEach(function(link) {
+        folioLinks.forEach(function (link)
+        {
             let modalbox = link.getAttribute('href');
             let instance = basicLightbox.create(
                 document.querySelector(modalbox),
                 {
-                    onShow: function(instance) {
+                    onShow: function (instance)
+                    {
                         //detect Escape key press
-                        document.addEventListener("keydown", function(event) {
+                        document.addEventListener("keydown", function (event)
+                        {
                             event = event || window.event;
-                            if (event.keyCode === 27) {
+                            if (event.keyCode === 27)
+                            {
                                 instance.close();
                             }
                         });
@@ -269,8 +298,10 @@
             modals.push(instance);
         });
 
-        folioLinks.forEach(function(link, index) {
-            link.addEventListener("click", function(event) {
+        folioLinks.forEach(function (link, index)
+        {
+            link.addEventListener("click", function (event)
+            {
                 event.preventDefault();
                 modals[index].show();
             });
@@ -279,23 +310,28 @@
     };  // end ssLightbox
 
 
-   /* Alert boxes
-    * ------------------------------------------------------ */
-    const ssAlertBoxes = function() {
+    /* Alert boxes
+     * ------------------------------------------------------ */
+    const ssAlertBoxes = function ()
+    {
 
         const boxes = document.querySelectorAll('.alert-box');
-  
-        boxes.forEach(function(box){
 
-            box.addEventListener('click', function(event) {
-                if (event.target.matches(".alert-box__close")) {
+        boxes.forEach(function (box)
+        {
+
+            box.addEventListener('click', function (event)
+            {
+                if (event.target.matches(".alert-box__close"))
+                {
                     event.stopPropagation();
                     event.target.parentElement.classList.add("hideit");
 
-                    setTimeout(function(){
+                    setTimeout(function ()
+                    {
                         box.style.display = "none";
                     }, 500)
-                }    
+                }
             });
 
         })
@@ -303,35 +339,40 @@
     }; // end ssAlertBoxes
 
 
-   /* Smoothscroll
-    * ------------------------------------------------------ */
-    const ssMoveTo = function(){
+    /* Smoothscroll
+     * ------------------------------------------------------ */
+    const ssMoveTo = function ()
+    {
 
         const easeFunctions = {
-            easeInQuad: function (t, b, c, d) {
+            easeInQuad: function (t, b, c, d)
+            {
                 t /= d;
                 return c * t * t + b;
             },
-            easeOutQuad: function (t, b, c, d) {
+            easeOutQuad: function (t, b, c, d)
+            {
                 t /= d;
-                return -c * t* (t - 2) + b;
+                return -c * t * (t - 2) + b;
             },
-            easeInOutQuad: function (t, b, c, d) {
-                t /= d/2;
-                if (t < 1) return c/2*t*t + b;
+            easeInOutQuad: function (t, b, c, d)
+            {
+                t /= d / 2;
+                if (t < 1) return c / 2 * t * t + b;
                 t--;
-                return -c/2 * (t*(t-2) - 1) + b;
+                return -c / 2 * (t * (t - 2) - 1) + b;
             },
-            easeInOutCubic: function (t, b, c, d) {
-                t /= d/2;
-                if (t < 1) return c/2*t*t*t + b;
+            easeInOutCubic: function (t, b, c, d)
+            {
+                t /= d / 2;
+                if (t < 1) return c / 2 * t * t * t + b;
                 t -= 2;
-                return c/2*(t*t*t + 2) + b;
+                return c / 2 * (t * t * t + 2) + b;
             }
         }
 
         const triggers = document.querySelectorAll('.smoothscroll');
-        
+
         const moveTo = new MoveTo({
             tolerance: 0,
             duration: 1200,
@@ -339,16 +380,18 @@
             container: window
         }, easeFunctions);
 
-        triggers.forEach(function(trigger) {
+        triggers.forEach(function (trigger)
+        {
             moveTo.registerTrigger(trigger);
         });
 
     }; // end ssMoveTo
 
 
-   /* Initialize
-    * ------------------------------------------------------ */
-    (function ssInit() {
+    /* Initialize
+     * ------------------------------------------------------ */
+    (function ssInit()
+    {
 
         ssPreloader();
         ssMobileMenu();
